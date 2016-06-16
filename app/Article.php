b4715f767d09bbcaf3c4fb43c9231c319e2f174b
cufrancis <cufrancis.com@gmail.com>
2016-06-16 22:55:51 +0800

    修改显示页面

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
   protected $fillable=['title', 'link', 'content', 'author_id'];
+  
+  public function authorInfo(){
+    return $this->hasOne('App\User', 'id', 'author_id');
+  }
 }
--- resources/views/themes/default/article/show.blade.php
+++ resources/views/themes/default/article/show.blade.php
@@ -4,6 +4,11 @@
 <h3><center><a href="{{ $article->link }}">{{$article->title}}</a></center></h3>
 
 <p>{{$article->content}} <small><em>(点击标题进入网站)</em></small></p>
+<hr />
+{{-- {{dd($article->authorInfo->name)}} --}}
+<p>Created_at {{ $article->updated_at }} </p>
+<p>updated_at:{{ $article->created_at }} </p>
+<p>Author: <a href="">{{ $article->authorInfo->name }}</a></p>
 @endsection
 
 @section('footer')
