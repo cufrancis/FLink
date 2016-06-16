e13305bd199141b1f44e1c4df19b2fe8a4720222
cufrancis <cufrancis.com@gmail.com>
2016-06-16 22:21:43 +0800

    litter things

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('theme::layout.public')
 
 @section('content')
 <div class="container">
--- resources/views/themes/default/home/index.blade.php
+++ resources/views/themes/default/home/index.blade.php
@@ -9,5 +9,5 @@
 @endsection
 
 @section('footer')
-    hello
+    
 @endsection
